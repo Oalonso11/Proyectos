dc7d241cf67696ce5876cda7255798bf780b561a
Oalonso11 <calinalonso00@outlook.com>
2024-09-17 09:34:21 -0600

    Add files via upload

---
+++ area_vol.c
@@ -0,0 +1,8 @@
+/*Programa que calcule el áera y volumen
+de una esfera, dado el valor del radio en cm.*/
+#include <stdio.h>
+#include <math.h>
+int main(){
+    
+    return 0;
+}
---
+++ velocidad_corre.c
@@ -0,0 +1,8 @@
+/*Programa para calcular la velocidad de los corredores
+en m/s de los corredores en una carrera de 1,500m*/
+#include <stdio.h>
+#include <math.h>
+int main(){
+    
+    return 0;
+}
