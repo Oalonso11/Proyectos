03ccc492c42b547af18c7b027cf09fe39e900808
Oalonso11 <calinalonso00@outlook.com>
2024-09-17 17:08:33 -0600

    Add files via upload

---
+++ area_vol.c
@@ -0,0 +1,13 @@
+//Programa que saca el volumen y area de una esfera
+#include <stdio.h>
+#include<math.h>
+int main(){
+  float radio, area, volumen;
+  printf("Inserte el radio en cm: ");
+  scanf("%f", &radio);
+  area = 4*M_PI*pow(radio,2);
+  printf("El área de la esfera es: %.3f cm^2\n", area);
+  volumen = 4*M_PI*pow(radio,3)/3;
+  printf("El volumen de la esfera es: %.3f cm^3", volumen); 
+  return 0;
+}
