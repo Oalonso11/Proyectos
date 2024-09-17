13889a6ff74349e0c1f9e13da56440394605bbba
Oalonso11 <calinalonso00@outlook.com>
2024-09-17 17:08:10 -0600

    Delete area_vol.c

--- area_vol.c
+++
@@ -1,9 +0,0 @@
-/*Programa que calcule el áera y volumen
-de una esfera, dado el valor del radio en cm.*/
-#include <stdio.h>
-#include <math.h>
-int main(){
- float radio, area, volum;
- 
- return 0;
-}
