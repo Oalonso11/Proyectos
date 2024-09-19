46150cca2bfd149f06dc22329d299ace10420c2d
Oalonso11 <calinalonso00@outlook.com>
2024-09-19 08:37:49 -0600

    Update velocidad_corre.c

--- velocidad_corre.c
+++ velocidad_corre.c
@@ -3,10 +3,17 @@ en m/s de los corredores en una carrera de 1,500m*/
 #include <stdio.h>
 #include <math.h>
 int main(){
-int distancia 
-distancia = 1500
-printf("Ingrese el tiempo total de carrera (horas, minutos, segundos): ")    
-    
+    const int distancia = 1500;
+    int min, seg;
+    float velocidad;
+    printf("Ingrese el tiempo total de minutos: ");
+    scanf("%d",&min);
+    min *= 60;
+    printf("Ingrese el timepo total de segundos: ");
+    scanf("%d",&seg);
+    seg += min;
+    velocidad = distancia/(float)seg;
+    printf("La velocidad del corredor es de: %.6f m/s",velocidad);
     
     return 0;
 }
